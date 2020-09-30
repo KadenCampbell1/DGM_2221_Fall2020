0f7e4c79786e292ddda21ed986f9b8b469b5f483
Kaden(LenLap) <10836246@my.uvu.edu>
2020-09-30 16:35:48 -0600

    Added WalkAndRun project

---
+++ UnitySilo/WalkAndRunProject/Assets/Scripts/WalkAndRun.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class WalkAndRun : MonoBehaviour
+{
+    public float moveSpeed, normalSpeed = 30f, fastSpeed = 50f;
+    private Rigidbody rBody;
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            moveSpeed = fastSpeed;
+        }
+        else if (Input.GetButtonUp("Fire3"))
+        {
+            moveSpeed = normalSpeed;
+        }
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * (moveSpeed * Time.deltaTime);
+        
+        rBody.MovePosition(gameObject.transform.position + movement);
+    }
+}
