d7647527019af85d48d42d0dd3de1a33f0e1c4af
Kaden(LenLap) <10836246@my.uvu.edu>
2020-10-22 22:26:42 -0600

    Added Shooting project and moved files around

---
+++ UnitySilo/TriggerProject/Assets/_Scripts/TriggerBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Threading;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class TriggerBehaviour : MonoBehaviour
+{
+    public Color newColor;
+    public Color defaultColor;
+    private MeshRenderer meshR;
+    private WaitForSeconds wfs;
+    public int holdTime = 10;
+    public GameObject door;
+    public TimerUI timer;
+
+    void Start()
+    {
+        meshR = GetComponent<MeshRenderer>();
+        meshR.material.color = defaultColor;
+        wfs = new WaitForSeconds(holdTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        newColor.a = 0.5f;
+        meshR.material.color = newColor;
+        door.SetActive(false);
+        
+        
+    }
+
+    private IEnumerator OnTriggerExit(Collider other)
+    { 
+        StartCoroutine(timer.Countdown());
+        yield return wfs;
+        meshR.material.color = defaultColor;
+        door.SetActive(true);
+    }
+    
+  
+}
