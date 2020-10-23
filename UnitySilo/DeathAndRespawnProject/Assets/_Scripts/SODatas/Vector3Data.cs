ff1e16a70d1c37c777a82e1ca10d554feb5800b7
Kaden(LenLap) <10836246@my.uvu.edu>
2020-10-23 10:01:52 -0600

    Added DeathAndRespawn project

---
+++ UnitySilo/DeathAndRespawnProject/Assets/_Scripts/SODatas/Vector3Data.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Data : MonoBehaviour
+{
+    public Vector3 value;
+
+    public void SetValueFromData(Vector3Data data)
+    {
+        value = data.value;
+    }
+}
