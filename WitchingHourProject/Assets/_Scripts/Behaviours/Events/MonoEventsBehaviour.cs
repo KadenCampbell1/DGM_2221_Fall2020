fc15ea6861639b9ac60e6e854a194d0ae3238456
Kaden(LenLap) <10836246@my.uvu.edu>
2020-11-08 20:03:14 -0700

    Added Unity Actions; began making rifts transport player

---
+++ WitchingHourProject/Assets/_Scripts/Behaviours/Events/MonoEventsBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventsBehaviour : MonoBehaviour
+{
+    public UnityEvent onEnableEvent, onStartEvent;
+
+    private void OnEnable()
+    {
+        onEnableEvent.Invoke();
+    }
+
+    private void Start()
+    {
+        onStartEvent.Invoke();
+    }
+}
