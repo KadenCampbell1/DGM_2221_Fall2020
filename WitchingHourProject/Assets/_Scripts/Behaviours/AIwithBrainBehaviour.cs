013ac319bd75a98ccad448cf34d9b9a3b1bafe33
Kaden(LenLap) <10836246@my.uvu.edu>
2020-11-17 22:53:15 -0700

    Worked on platforms and AI

---
+++ WitchingHourProject/Assets/_Scripts/Behaviours/AIwithBrainBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class AIwithBrainBehaviour : MonoBehaviour
+{
+    public AIBrainBaseData brain;
+    
+    private NavMeshAgent agent;
+    private MeshFilter filter;
+    private MeshRenderer render;
+    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        filter = GetComponent<MeshFilter>();
+        render = GetComponent<MeshRenderer>();
+        filter.mesh = brain.mesh;
+        render.material = brain.material;
+        brain.Patrol(agent);
+    }
+}
---
+++ WitchingHourProject/Assets/_Scripts/ScriptableObjectsScripts/AIBrainBaseData.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu]
+public class AIBrainBaseData : ScriptableObject
+{
+    public float health, speed, rotateSpeed, damage;
+    public bool canShoot, canPatrol, canHunt;
+    public Material material;
+    public Mesh mesh;
+    public List<Transform> patrolPoints;
+    
+    private int i = 0;
+    
+    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    private WaitForSeconds wfs = new WaitForSeconds(2f);
+    
+
+    public virtual IEnumerator Patrol(NavMeshAgent agent)
+    {
+        canPatrol = true;
+        canHunt = false;
+        while (canPatrol)
+        {
+            yield return wffu;
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                yield return wfs;
+                agent.destination = patrolPoints[i].position;
+                i = (i + 1) % patrolPoints.Count;
+            }
+        }
+    }
+
+    public virtual IEnumerator Hunt(NavMeshAgent agent, Transform obj)
+    {
+        canHunt = true;
+        canPatrol = false;
+        while (canHunt)
+        {
+            yield return wffu;
+            agent.destination = obj.position;
+        }
+    }
+
+}
