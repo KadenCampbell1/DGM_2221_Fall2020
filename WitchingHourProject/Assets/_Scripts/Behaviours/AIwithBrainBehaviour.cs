47741276942b066145f2f80664e7a9970b0357fd
Kaden(LenLap) <10836246@my.uvu.edu>
2020-11-30 20:44:13 -0700

    Worked on AI

--- WitchingHourProject/Assets/_Scripts/Behaviours/AIwithBrainBehaviour.cs
+++ WitchingHourProject/Assets/_Scripts/Behaviours/AIwithBrainBehaviour.cs
@@ -5,23 +5,83 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshRenderer))]
 public class AIwithBrainBehaviour : MonoBehaviour
 {
     public AIBrainBaseData brain;
     
     private NavMeshAgent agent;
-    private MeshFilter filter;
-    private MeshRenderer render;
+    public bool canPatrol, canHunt;
+    private float health, speed, rotateSpeed, damage, baseOffset, timer;
+
+    private int i = 0;
     
+    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    private WaitForSeconds wfs;
+
+    public GameObject obj;
     
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        filter = GetComponent<MeshFilter>();
-        render = GetComponent<MeshRenderer>();
+        RefreshBools();
+        health = brain.health;
+        speed = brain.speed;
+        rotateSpeed = brain.rotateSpeed;
+        damage = brain.damage;
+        baseOffset = brain.baseOffset;
+        timer = brain.timer;
+        wfs = wfs = new WaitForSeconds(timer);
         Instantiate<GameObject>(brain.artPrefab, this.transform);
-        brain.Patrol(agent);
+        StartCoroutine(Patrol());
+    }
+
+    public void RefreshBools()
+    {
+        canPatrol = brain.canPatrol;
+        canHunt = brain.canHunt;
+    }
+
+    private IEnumerator ChangeBools()
+    {
+        var newTime = new WaitForSeconds(timer * 2);
+        yield return newTime;
+        canPatrol =! canPatrol;
+        canHunt = !canHunt;
+    }
+
+    public void StartRoutines()
+    {
+        StartCoroutine(ChangeBools());
+    }
+
+    private IEnumerator Patrol()
+    {
+        canPatrol = true;
+        canHunt = false;
+        while (canPatrol)
+        {
+            yield return wffu;
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                yield return wfs;
+                agent.destination = brain.patrolPoints[i].position;
+                i = (i + 1) % brain.patrolPoints.Count;
+            }
+        }
+
+        StartCoroutine(Hunt(obj.transform));
+    }
+
+    private IEnumerator Hunt(Transform obj)
+    {
+        canHunt = true;
+        canPatrol = false;
+        while (canHunt)
+        {
+            yield return wffu;
+            agent.destination = obj.position;
+        }
+
+        StartCoroutine(Patrol());
     }
 }
--- WitchingHourProject/Assets/_Scripts/ScriptableObjectsScripts/AIBrainBaseData.cs
+++ WitchingHourProject/Assets/_Scripts/ScriptableObjectsScripts/AIBrainBaseData.cs
@@ -6,44 +6,8 @@ using UnityEngine.AI;
 [CreateAssetMenu]
 public class AIBrainBaseData : ScriptableObject
 {
-    public float health, speed, rotateSpeed, damage;
+    public float health, speed, rotateSpeed, damage, baseOffset, timer;
     public bool canShoot, canPatrol, canHunt;
-    public Material material;
-    public Mesh mesh;
     public List<Transform> patrolPoints;
     public GameObject artPrefab;
-    
-    private int i = 0;
-    
-    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
-    private WaitForSeconds wfs = new WaitForSeconds(2f);
-    
-
-    public virtual IEnumerator Patrol(NavMeshAgent agent)
-    {
-        canPatrol = true;
-        canHunt = false;
-        while (canPatrol)
-        {
-            yield return wffu;
-            if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            {
-                yield return wfs;
-                agent.destination = patrolPoints[i].position;
-                i = (i + 1) % patrolPoints.Count;
-            }
-        }
-    }
-
-    public virtual IEnumerator Hunt(NavMeshAgent agent, Transform obj)
-    {
-        canHunt = true;
-        canPatrol = false;
-        while (canHunt)
-        {
-            yield return wffu;
-            agent.destination = obj.position;
-        }
-    }
-
 }
