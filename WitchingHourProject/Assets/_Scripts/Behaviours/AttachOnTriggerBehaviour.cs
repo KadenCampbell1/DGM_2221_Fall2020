9141bf18efbb05938a1e0c1a6f37ab065489e282
Kaden(LenLap) <10836246@my.uvu.edu>
2020-12-02 23:44:06 -0700

    Changed compare tags to IDs; tried rearranging rigidbody to fix sticking

---
+++ WitchingHourProject/Assets/_Scripts/Behaviours/AttachOnTriggerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AttachOnTriggerBehaviour : MonoBehaviour
+{
+    public UnityEvent onPlatformEvent, offPlatformEvent;
+    public IDData idObj;
+    private void OnTriggerEnter(Collider other)
+    {
+        var otherID = other.GetComponent<IDHolderBehaviour>();
+        if (otherID == null) return;
+
+        if (idObj == otherID.idObj)
+        {
+            transform.parent = other.transform;
+        }
+        onPlatformEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent = null;
+        offPlatformEvent.Invoke();
+    }
+}
---
+++ WitchingHourProject/Assets/_Scripts/Behaviours/IDHolderBehaviour.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IDHolderBehaviour : MonoBehaviour
+{
+    public IDData idObj;
+}
