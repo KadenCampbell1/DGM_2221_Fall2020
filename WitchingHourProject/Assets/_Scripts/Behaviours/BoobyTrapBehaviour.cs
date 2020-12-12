15c69e67f270f6abdb45726d98f9d6d86a18aeb4
Kaden(LenLap) <10836246@my.uvu.edu>
2020-12-12 15:52:48 -0700

    Added Second and final puzzles; Game should be finished

---
+++ WitchingHourProject/Assets/_Scripts/Behaviours/BoobyTrapBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BoobyTrapBehaviour : MonoBehaviour
+{
+    public GameAction floatActionHandler;
+    public float damageValue;
+    public UnityEvent causedDamageEvent;
+    private bool wasTriggered;
+
+    private void Start()
+    {
+        floatActionHandler.floatDataAction += CauseDamageToValue;
+    }
+
+    public void WasTriggeredTrue()
+    {
+        wasTriggered = true;
+    }
+
+    private void CauseDamageToValue(FloatData obj)
+    {
+        if (wasTriggered)
+        {
+            obj.IncrementValue(damageValue);
+            causedDamageEvent.Invoke();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        floatActionHandler.floatDataAction -= CauseDamageToValue;
+    }
+}
---
+++ WitchingHourProject/Assets/_Scripts/Behaviours/PickUpBlockBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpBlockBehaviour : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            transform.parent = other.transform;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            transform.parent = null;
+        }
+    }
+}
